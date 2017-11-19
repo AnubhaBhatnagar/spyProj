f7cb08e5c260a41c7f4c3982491717426b7dc349
Anubha17 <2014anubhabhatnagar@gmail.com>
2017-11-19 15:35:27 +0530

    added new files

---
+++ spy_details.py
@@ -0,0 +1,4 @@
+spy_name="Anubha"
+spy_saluatation="Ms"
+spy_age=18
+spy_rating=10
---
+++ spychat.py
@@ -0,0 +1,49 @@
+from spy_details import spy_name,spy_age,spy_saluatation,spy_rating
+
+print 'Let\'s get started'
+question = "Continue  as "+spy_saluatation+" "+spy_name+"(Y/N)"
+#print('Hey Jus checking if this ever comes here')
+existing = raw_input(question)
+
+def start_chat(spy_name,spy_age,spy_rating):
+    menu_choices ="What do u want o do?\n 1:Add a status update\n"
+    menu_choices =int (raw_input(menu_choices))
+    print("what's up bro?")
+    #if(menu_choice==1):
+        #print("Yo")
+        #add Status Update
+    #else:
+        #Exit application
+
+
+
+if existing.upper()=="Y":
+    print "Authentication complete.Welcome"+spy_name+"age:"+str(spy_age)+"and rating:"+str(spy_rating)+"Proud to have you on leader board"
+
+else:
+    spy_name= raw_input("Welcome to spy chat, you must tell your spy name first:")
+    if len(spy_name)>0:
+        spy_saluatation= raw_input("What should we call u(Mr or Ms)?")
+        if len(spy_saluatation)>0:
+            spy_name=spy_saluatation+" "+spy_name
+            spy_age=input("what is your spy age?")
+            if spy_age>12 and spy_age<50:
+                spy_rating=input("what is your spy rating?")
+
+                if spy_rating>4.5:
+                    print "GREAT ACE"
+                elif spy_rating>3.5 and spy_rating<=4.5:
+                    print "You are one of the good ones"
+                elif spy_rating>=2.5 and spy_rating<=3.5:
+                    print "you can always do better"
+                else:
+                    print "we can always use somebody to help in he office"
+                spy_is_online = True
+                print "Authentication complete.welcome "+spy_name+"age:"+str(spy_age)+"and rating of:"+str(spy_rating)+"Proud to have you on leaderboard"
+                start_chat(spy_name,spy_age,spy_rating)
+            else:
+                print"sorry u r not age to be a spy"
+
+            print"a spy needs to have a valid name.Try again please. "
+                    
+
